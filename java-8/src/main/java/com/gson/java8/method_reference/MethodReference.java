e85a9264bd346522440b2b9d63cc954e77ac6fb3
ruchen <ruchen.gs@alibaba-inc.com>
2019-08-18 00:21:59 +0800

    add java-8

--- java-8/src/main/java/com/gson/java8/method_reference/MethodReference.java
+++ java-8/src/main/java/com/gson/java8/method_reference/MethodReference.java
@@ -1,16 +1,40 @@
 package com.gson.java8.method_reference;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class MethodReference {
 
     /**
-     * 方法引用，静态方法中，只能引用静态方法
+     * 方法引用永远是可以  转化为  lamda表达式的
      * @param args
      */
     public static void main(String[] args) {
-        Function<Integer, ?> func = Product::get;
         Supplier<Product> supplier = Product::new;
+        Product product1 = supplier.get();
+        Product product2 = supplier.get();
+        List<Product> products = Arrays.asList(product1, product2);
+
+        // 这叫做方法引用
+        products.forEach(System.out::println);
+        // 这叫做lamda表达式，与上面是等价的
+        products.forEach(x -> System.out.println(x));
+
+        // 以下两者等价
+        products.forEach(product1::getPrice);
+        products.forEach(product3 -> product1.getPrice(product3));
+
+
+        //以下两者等价
+        products.forEach(Product::repair);
+        products.forEach(product -> product.repair());
+
+        // 以下两者等价，是不可编译的
+        // products.forEach(Product::getPrice);
+        // products.forEach(product -> Product.getPrice(product));
+
     }
 }
--- java-8/src/main/java/com/gson/java8/method_reference/Product.java
+++ java-8/src/main/java/com/gson/java8/method_reference/Product.java
@@ -1,8 +1,21 @@
 package com.gson.java8.method_reference;
 
+import java.util.Random;
+
 public class Product {
 
-    public static String get(Integer i){
-        return  i + "——";
+    private int price;
+
+    public Product() {
+        this.price = new Random().nextInt();
+        System.out.println("Product 构造器执行, price = " + price);
+    }
+
+    public void getPrice(Product product) {
+        System.out.println("price =" + product.price);
+    }
+
+    public void repair(){
+
     }
 }
