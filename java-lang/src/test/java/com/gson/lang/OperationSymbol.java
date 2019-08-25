0dbc59e792f9b807465ab55be92d1da1806a199f
ruchen <ruchen.gs@alibaba-inc.com>
2019-08-25 22:05:36 +0800

    add java-8

---
+++ java-lang/src/test/java/com/gson/lang/OperationSymbol.java
@@ -0,0 +1,17 @@
+package com.gson.lang;
+
+import org.junit.Test;
+
+/**
+ * 运算符号
+ */
+public class OperationSymbol {
+
+    @Test
+    public void test1(){
+        int a = 7; //  111
+        int b = 9; // 1001
+
+        System.out.println(a&b); //位与   1
+    }
+}
