4240014d181e8cb522d67460a411d716ecc9e0e3
ruchen <ruchen.gs@alibaba-inc.com>
2019-08-25 22:08:41 +0800

    add java-8

--- java-spring/src/test/java/com/gson/spring/olympic/OlympicTests.java
+++ java-spring/src/test/java/com/gson/spring/olympic/OlympicTests.java
@@ -11,9 +11,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * 引入spring-test包
  * 利用Runwith
  * 利用ContextConfiguration 支持(多个)配置文件和(多个)配置类
+ *
+ * 可以读取main/resources目录和test/resources目录下的资源文件
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(value="classpath:olympic_beans.xml")
+@ContextConfiguration(value="classpath:olympic_beans_test.xml")
 public class OlympicTests {
 
     @Autowired
