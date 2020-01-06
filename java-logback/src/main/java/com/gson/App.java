bdbafc7e02f5f00b2483931c2ec4da36628a034e
ruchen <ruchen.gs@alibaba-inc.com>
2020-01-06 18:34:03 +0800

    add java fastjson

---
+++ java-logback/src/main/java/com/gson/App.java
@@ -0,0 +1,21 @@
+package com.gson;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private static Logger logger = LoggerFactory.getLogger(App.class);
+    public static void main( String[] args ) throws UnsupportedEncodingException {
+        String info = "高嵩";
+        byte[] bytes = info.getBytes("UTF-8");
+        String s = new String(bytes);
+        logger.debug(s);
+    }
+}
