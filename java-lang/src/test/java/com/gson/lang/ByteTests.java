b3233027aa65436b144fe2d25cbfba57badd58e3
ruchen <ruchen.gs@alibaba-inc.com>
2020-12-11 00:26:37 +0800

    fix

--- java-lang/src/test/java/com/gson/lang/ByteTests.java
+++ java-lang/src/test/java/com/gson/lang/ByteTests.java
@@ -1,5 +1,8 @@
 package com.gson.lang;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import java.util.Arrays;
 
 public class ByteTests {
@@ -14,5 +17,26 @@ public class ByteTests {
 
         System.out.println(Arrays.toString(a));
         System.out.println(a);
+    }
+
+    @Test
+    public void testBit(){
+        System.out.println(1 << 2);
+
+        //相同位置都为1，结果才为1
+        // B11110 & B10000 = B10000=16
+        System.out.println(30 & 1 << 4);
+
+        //B11110 & B01000 = B01000 = 8
+        System.out.println(30 & 1<< 3);
+    }
+
+    @Test
+    public void express(){
+        byte a = 0b0000001000;
+        Assert.assertEquals(a, 8);
+
+        byte b = 0b100_0000;
+        Assert.assertEquals(b, 64);
     }
 }
