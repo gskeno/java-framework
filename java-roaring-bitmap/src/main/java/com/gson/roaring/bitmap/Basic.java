1daa6572669a8c8ce7849f25840c8cc023a74884
ruchen <ruchen.gs@alibaba-inc.com>
2022-02-27 19:32:02 +0800

    test case

--- java-roaring-bitmap/src/main/java/com/gson/roaring/bitmap/Basic.java
+++ java-roaring-bitmap/src/main/java/com/gson/roaring/bitmap/Basic.java
@@ -1,20 +1,20 @@
 package com.gson.roaring.bitmap;
 
-import org.roaringbitmap.Container;
-import org.roaringbitmap.ContainerPointer;
-import org.roaringbitmap.RoaringArray;
 import org.roaringbitmap.RoaringBitmap;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.Arrays;
-import java.util.Objects;
 
 public class Basic {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // RoaringBitmap.RoaringArray keys 数组
         // 由于1，2，3，1000 四个元素的高16位都是0，所以keys数组只需要一个元素
-        int[] elements = {1,2,3,1000, Integer.MAX_VALUE - 1};
+        int[] elements = {1, 2, 3, 1000, Integer.MAX_VALUE - 1};
         RoaringBitmap rr = RoaringBitmap.bitmapOf(elements);
 
+        boolean b1 = rr.runOptimize();
+        System.out.println("exit run-length optimize:" + b1);
 
 
         // would return the third value or 1000
@@ -39,7 +39,7 @@ public class Basic {
         System.out.println("7 contains in " + Arrays.toString(elements) + " :" + contains);
 
         RoaringBitmap rr2 = new RoaringBitmap();
-        rr2.add(4000L,4255L);
+        rr2.add(4000L, 4255L);
         // new bitmap
         RoaringBitmap rror = RoaringBitmap.or(rr, rr2);
         // in-place computation
@@ -47,13 +47,31 @@ public class Basic {
         rr.or(rr2);
         // true
         boolean equals = rror.equals(rr);
-        if(!equals) throw new RuntimeException("bug");
+        if (!equals) throw new RuntimeException("bug");
         // number of values stored?
         long cardinality = rr.getLongCardinality();
         System.out.println("elements  cardinality " + cardinality);
         // a "forEach" is faster than this loop, but a loop is possible:
-        for(int i : rr) {
-             System.out.println(i);
+        for (int i : rr) {
+            System.out.println(i);
         }
+
+        // 是否存在步长优化, 有RunContainer的时候，才会有步长优化
+        System.out.println("before runOptimize, serializedSizeInBytes=" + rr.serializedSizeInBytes());
+        rr.runOptimize();
+        // 获取需要的字节大小，看源码
+        System.out.println("after runOptimize, serializedSizeInBytes=" + rr.serializedSizeInBytes());
+
+        // 序列化到byte数组中去
+        ByteBuffer byteBuffer = ByteBuffer.allocate(rr.serializedSizeInBytes());
+        rr.serialize(byteBuffer);
+        byte[] array = byteBuffer.array();
+        System.out.println(array.length);
+
+        // byte[] ---> ByteBuffer ---> RoaringBitmap
+        RoaringBitmap bitmap = new RoaringBitmap();
+        bitmap.deserialize(ByteBuffer.wrap(array));
+        // 序列化之后，再反序列化，两者元素是一致的，所以equals
+        System.out.println(bitmap.equals(rr));
     }
 }
