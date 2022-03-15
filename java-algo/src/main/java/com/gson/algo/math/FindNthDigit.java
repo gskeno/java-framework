d1b1103575c2462d973e14ed251c8141820c3671
ruchen <ruchen.gs@alibaba-inc.com>
2022-03-15 14:06:47 +0800

    test case

--- java-algo/src/main/java/com/gson/algo/math/FindNthDigit.java
+++ java-algo/src/main/java/com/gson/algo/math/FindNthDigit.java
@@ -12,65 +12,61 @@ package com.gson.algo.math;
  * 第 13 位是 1 ，以此类题，请你输出第 n 位对应的数字。
  */
 public class FindNthDigit {
-    /**
-     * @return
-     */
-    public int findNthDigit(int n) {
-        // 1位数字用尽时占用的数字字符串长度为 10
-        // 2位数字用尽时占用的数字字符串长度为 10 + (10^2-10^1)*2 = 190
-        // 3位数字用尽时占用的数字字符串长度为 10 + (10^2-10^1)*2 + （10^3-10^2)*3 = 190 + 2700 = 2890
-        // 4位数字用尽时占用的数字字符串长度为 10 + (10^2-10^1)*2 + （10^3-10^2)*3 +（10^4-10^3)*4
-        // 那么数字字符串下标为n时，当时表示的是几位数字呢？
-        int needDigit = 1;
-        int lengthOnNeedDigit = 10;
-        int nextDigit = 2;
-        int lengthOnNextDigit = 10 + (100-10)*2;
-        for (; (n+1) > lengthOnNeedDigit; ) {
-            int temp = lengthOnNextDigit;
-            lengthOnNextDigit += ((int) Math.pow(10, nextDigit + 1) - (int) Math.pow(10, nextDigit)) * (nextDigit + 1);
-            lengthOnNeedDigit = temp;
-            needDigit++;
-            nextDigit++;
-        }
-        return needDigit;
-    }
 
 
     public int findNthDigit2(int n) {
-        // 1位数字用尽时占用的数字字符串长度为 10
-        // 2位数字用尽时占用的数字字符串长度为 10 + (10^2-10^1)*2 = 190
-        // 3位数字用尽时占用的数字字符串长度为 10 + (10^2-10^1)*2 + （10^3-10^2)*3 = 190 + 2700 = 2890
-        // 4位数字用尽时占用的数字字符串长度为 10 + (10^2-10^1)*2 + （10^3-10^2)*3 +（10^4-10^3)*4
-        // 那么数字字符串下标为n时，当时表示的是几位数字呢？
-        int needDigit = 1;
-        int lengthOnNeedDigit = 10;
-        int nextDigit = 2;
-        int lengthOnNextDigit = 10 + (100-10)*2;
-        for (; (n+1) > lengthOnNeedDigit; ) {
-            int temp = lengthOnNextDigit;
-            lengthOnNextDigit += ((int) Math.pow(10, nextDigit + 1) - (int) Math.pow(10, nextDigit)) * (nextDigit + 1);
-            lengthOnNeedDigit = temp;
-            needDigit++;
-            nextDigit++;
+        if (n == 0){
+            return 0;
         }
+        int needDigit = 1;
+        long baseSegmentLen = 0;
+        while (true){
+            long numsCount = needDigit == 1? 10: (int)Math.pow(10, needDigit-1) * 9 * needDigit;
+            if (baseSegmentLen + numsCount >= n){
+                break;
+            }
+            needDigit++;
+            baseSegmentLen += numsCount;
+        }
+        System.out.println("needDigit=" + needDigit);
+        // needDigit位数字所能表达的最小数字，如
+        // 1位数字的最小数字是0
+        // 2位数字的最小数字是10
+        // 3位数字的最小数字是100
+        int firstNum = needDigit == 1 ? 0 : (int)Math.pow(10, (needDigit-1));
 
-        // lengthOnNeedDigit- (int) Math.pow(10, needDigit) 表示 needDigit-1位数字所能表达的数字串的长度
-        //
-        int m = (n+1) - (lengthOnNeedDigit- (int) Math.pow(10, needDigit));
+        int m = (int)(n - baseSegmentLen);
         int p = m/needDigit;
         int q = m%needDigit;
-        char goal = String.valueOf(Math.pow(10, (needDigit-1)) + p).charAt(q);
+
+        char goal = String.valueOf(firstNum + p).charAt(q);
         return Integer.valueOf(String.valueOf(goal));
     }
 
     public static void main(String[] args) {
         FindNthDigit findNthDigit = new FindNthDigit();
-        System.out.println(findNthDigit.findNthDigit(0));
-        System.out.println(findNthDigit.findNthDigit(9));
-        System.out.println(findNthDigit.findNthDigit(10));
-        System.out.println(findNthDigit.findNthDigit(189));
-        System.out.println(findNthDigit.findNthDigit(190));
-        System.out.println(findNthDigit.findNthDigit(2889));
-        System.out.println(findNthDigit.findNthDigit(2890));
+//        System.out.println(findNthDigit.findNthDigit(0));
+//        System.out.println(findNthDigit.findNthDigit(9));
+//        System.out.println(findNthDigit.findNthDigit(10));
+//        System.out.println(findNthDigit.findNthDigit(189));
+//        System.out.println(findNthDigit.findNthDigit(190));
+//        System.out.println(findNthDigit.findNthDigit(2889));
+//        System.out.println(findNthDigit.findNthDigit(2890));
+//        System.out.println(findNthDigit.findNthDigit2(0));
+//        System.out.println(findNthDigit.findNthDigit2(9));
+//        System.out.println(findNthDigit.findNthDigit2(10));
+//        System.out.println(findNthDigit.findNthDigit2(11));
+//        System.out.println(findNthDigit.findNthDigit2(12));
+//        System.out.println(findNthDigit.findNthDigit2(13));
+//        System.out.println(findNthDigit.findNthDigit2(14));
+//        System.out.println(findNthDigit.findNthDigit2(15));
+//        System.out.println(findNthDigit.findNthDigit2(16));
+//        System.out.println(findNthDigit.findNthDigit2(17));
+//        System.out.println(findNthDigit.findNthDigit2(189));
+//        System.out.println(findNthDigit.findNthDigit2(190));
+//        System.out.println(findNthDigit.findNthDigit2(191));
+//        System.out.println(findNthDigit.findNthDigit2(192));
+//        System.out.println(findNthDigit.findNthDigit2(193));
+        System.out.println(findNthDigit.findNthDigit2(1000000000));
     }
 }
