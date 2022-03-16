df11d9a0e6c71a26b44d11353407c1047e2d8529
ruchen <ruchen.gs@alibaba-inc.com>
2022-03-16 14:11:36 +0800

    test case

---
+++ java-algo/src/main/java/com/gson/algo/dynamic/plan/MaxSumSubArrayMaxLen.java
@@ -0,0 +1,20 @@
+package com.gson.algo.dynamic.plan;
+
+/**
+ * https://www.nowcoder.com/practice/11662ff51a714bbd8de809a89c481e21
+ * 连续子数组最大和，求对应的子数组，如果子数组存在多个，返回长度最长的那一个
+ *
+ * 输入：
+ * [1,-2,3,10,-4,7,2,-5]
+ * 返回值：
+ * [3,10,-4,7,2]
+ */
+public class MaxSumSubArrayMaxLen {
+    public int[] maxSumSubArrayMaxLen(int[] array) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
