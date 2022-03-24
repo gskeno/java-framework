15156ff757686703db776ac0f69c7f746767ea07
ruchen <ruchen.gs@alibaba-inc.com>
2022-03-24 19:59:03 +0800

    add solr

---
+++ java-springboot-solr/src/main/java/com/gsonkeno/javaspringbootsolr/JavaSpringbootSolrApplication.java
@@ -0,0 +1,13 @@
+package com.gsonkeno.javaspringbootsolr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootSolrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringbootSolrApplication.class, args);
+    }
+
+}
