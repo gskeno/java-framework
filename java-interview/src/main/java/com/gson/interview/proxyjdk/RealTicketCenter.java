ccf455b86a104d425f0fe91cd9da6ca7a29525c9
ruchen <ruchen.gs@alibaba-inc.com>
2022-03-28 22:23:00 +0800

    成对反转链表

---
+++ java-interview/src/main/java/com/gson/interview/proxyjdk/RealTicketCenter.java
@@ -0,0 +1,19 @@
+package com.gson.interview.proxyjdk;
+
+public class RealTicketCenter implements TicketCenter {
+
+    @Override
+    public void buyTicket(Integer amount) {
+        System.out.println("buyTicket,need ￥" + amount);
+    }
+
+    @Override
+    public void refundTicket() {
+        System.out.println("refundTicket");
+    }
+
+    @Override
+    public void sellTicket() {
+        System.out.println("sellTicket");
+    }
+}
---
+++ java-interview/src/main/java/com/gson/interview/proxyjdk/TicketCenter.java
@@ -0,0 +1,7 @@
+package com.gson.interview.proxyjdk;
+
+public interface TicketCenter {
+    void buyTicket(Integer amount);
+    void refundTicket();
+    void sellTicket();
+}
