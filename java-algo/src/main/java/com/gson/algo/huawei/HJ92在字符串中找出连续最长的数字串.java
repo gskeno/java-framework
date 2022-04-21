46cf6d1b23b862a46f19589986dc4eb42abd76eb
ruchen <ruchen.gs@alibaba-inc.com>
2022-04-21 20:31:53 +0800

    找连续最长数字串-华为机试

--- java-algo/src/main/java/com/gson/algo/huawei/HJ92在字符串中找出连续最长的数字串.java
+++ java-algo/src/main/java/com/gson/algo/huawei/HJ92在字符串中找出连续最长的数字串.java
@@ -10,65 +10,56 @@ public class HJ92在字符串中找出连续最长的数字串 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        while(scanner.hasNext()) {
+        while (scanner.hasNext()) {
             String line = scanner.nextLine();
             String result = getStrAndMaxLen(line);
             System.out.println(result);
         }
-
     }
 
-    public static String getStrAndMaxLen(String line){
-            int maxLength = 0;
-            StringBuilder sb = new StringBuilder();
+    public static String getStrAndMaxLen(String line) {
+        int maxLength = 0;
+        StringBuilder sb = new StringBuilder();
 
-            int len = 0;
-            StringBuilder tempSb = new StringBuilder();
-            for(char c : line.toCharArray()){
-                if (c - '0' >= 0 && '9' - c >=0){
-                    len++;
-                    tempSb.append(c);
-                }
-                // 遇到一个数字串后的第一个非数字字符,且该数字串长于当前已遍历的最长数字串
-                else if (len > 0 && len > maxLength){
-                    if (sb.length() > 0){
-                        sb.delete(0, sb.length());
-                    }
-                    maxLength = len;
-                    sb.append(tempSb);
-                    if (tempSb.length() > 0){
-                        tempSb.delete(0, tempSb.length());
-                        len = 0;
-                    }
-                }
-                // 遇到一个数字串后的第一个非数字字符,且该数字串等于当前已遍历的最长数字串
-                else if (len > 0 && len == maxLength){
-                    sb.append(tempSb);
-                    if (tempSb.length() > 0){
-                        tempSb.delete(0, tempSb.length());
-                        len = 0;
-                    }
-                }
-                // 遇到一个数字串后的第一个非数字字符,且该数字串短于当前已遍历的最长数字串
-                else if (len > 0 && len < maxLength){
-                    if (tempSb.length() > 0){
-                        tempSb.delete(0, tempSb.length());
-                        len = 0;
-                    }
-                }
+        int len = 0;
+        StringBuilder tempSb = new StringBuilder();
+        for (char c : line.toCharArray()) {
+            if (c - '0' >= 0 && '9' - c >= 0) {
+                len++;
+                tempSb.append(c);
             }
-
-            if (len > 0 && len > maxLength) {
-                if (sb.length() > 0) {
-                    sb.delete(0, sb.length());
-                }
+            // 遇到一个数字串后的第一个非数字字符,且该数字串长于当前已遍历的最长数字串
+            else if (len > 0 && len > maxLength) {
+                sb.delete(0, sb.length());
                 maxLength = len;
-            }
 
-            if (len > 0 && len == maxLength){
                 sb.append(tempSb);
-            }
+                tempSb.delete(0, tempSb.length());
+                len = 0;
 
-            return sb.append(",").append(maxLength).toString();
+            }
+            // 遇到一个数字串后的第一个非数字字符,且该数字串等于当前已遍历的最长数字串
+            else if (len > 0 && len == maxLength) {
+                sb.append(tempSb);
+                tempSb.delete(0, tempSb.length());
+                len = 0;
+            }
+            // 遇到一个数字串后的第一个非数字字符,且该数字串短于当前已遍历的最长数字串
+            else if (len > 0 && len < maxLength) {
+                tempSb.delete(0, tempSb.length());
+                len = 0;
+            }
+        }
+
+        if (len > 0 && len > maxLength) {
+            sb.delete(0, sb.length());
+            maxLength = len;
+        }
+
+        if (len > 0 && len == maxLength) {
+            sb.append(tempSb);
+        }
+
+        return sb.append(",").append(maxLength).toString();
     }
 }
--- java-algo/src/test/java/com/gson/algo/huawei/Hj92Test.java
+++ java-algo/src/test/java/com/gson/algo/huawei/Hj92Test.java
@@ -8,5 +8,9 @@ public class Hj92Test {
         String line = "a8a72a6a5yy98y65ee1r2";
         String result = HJ92在字符串中找出连续最长的数字串.getStrAndMaxLen(line);
         System.out.println(result);
+        line = "abcd12345ed125ss123058789";
+        result = HJ92在字符串中找出连续最长的数字串.getStrAndMaxLen(line);
+        System.out.println(result);
+
     }
 }
