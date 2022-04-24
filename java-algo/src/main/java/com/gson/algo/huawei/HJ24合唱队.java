b43829b36d3804cc0c3682a323a68a7f1d025fb6
ruchen <ruchen.gs@alibaba-inc.com>
2022-04-24 10:25:46 +0800

    找连续最长数字串-华为机试

--- java-algo/src/main/java/com/gson/algo/huawei/HJ24合唱队.java
+++ java-algo/src/main/java/com/gson/algo/huawei/HJ24合唱队.java
@@ -59,4 +59,68 @@ public class HJ24合唱队 {
 
         return heights.length - maxLen;
     }
+
+    public static int getEvictNums2(int n, int[] arr){
+        int[] left = new int[n]; //存储每个数左边小于其的数的个数
+        int[] right = new int[n];//存储每个数右边小于其的数的个数
+        left[0] = arr[0];
+        right[n - 1] = arr[n-1];
+        //记录以i为终点的从左向右和从右向左走的子序列元素个数
+        int num[] =new  int [n];
+        //记录当前子序列的长度
+        int index = 1;
+        for(int i=1;i<n;i++){
+            if(arr[i]>left[index-1]){
+                //直接放在尾部，i左侧元素个数
+                num[i] = index;
+                //更新递增序列
+                left[index++] = arr[i];
+            }else {
+                //找到当前元素应该放在的位置
+                int low = 0,high = index-1;
+                while(low < high){
+                    int mid = (low+high)/2;
+                    if(left[mid] <arr[i]){
+                        low = mid + 1;
+
+                    }
+                    else{
+                        high = mid;
+                    }
+                }
+
+                // 下面这两行是精髓所在
+                // 将所属位置替换为当前元素
+                left[low] = arr[i];
+                // 当前位置i的左侧元素个数
+                num[i] = low;
+            }
+        }
+        index = 1;
+        for(int i=n-2;i>=0;i--){
+            if(arr[i]>right[index-1]){
+                num[i] += index;
+                right[index++] = arr[i];
+            }else {
+                int low = 0,high = index-1;
+                while(low < high){
+                    int mid = (high+low)/2;
+                    if(right[mid]<arr[i]){
+                        low = mid+1;
+                    }
+                    else {
+                        high = mid;
+                    }
+                }
+                right[low] = arr[i];
+                num[i]+=low;
+            }
+        }
+        int max = 1;
+        for (int number: num ){
+            max = Math.max(max,number);
+        }
+        // max+1为最大的k
+        return  n - max;
+    }
 }
--- java-algo/src/test/java/com/gson/algo/huawei/Hj24Test.java
+++ java-algo/src/test/java/com/gson/algo/huawei/Hj24Test.java
@@ -18,4 +18,12 @@ public class Hj24Test {
         String[] s1 = s.split(" ");
         System.out.println(Arrays.toString(s1));
     }
+
+    @Test
+    public void test2(){
+        int n = 8;
+        int[] nums = new int[]{186, 186, 150, 200, 160, 130, 197, 200};
+        int evictNums2 = HJ24合唱队.getEvictNums2(n, nums);
+        System.out.println(evictNums2);
+    }
 }
