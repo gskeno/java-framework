1ecb6bf56ff14fc93f498789b843afc34a3cf5d8
ruchen <ruchen.gs@alibaba-inc.com>
2022-06-27 14:03:06 +0800

    算法训练

---
+++ java-algo/src/main/java/com/gson/algo/leetcode/top100/二叉树的最近公共祖先_236.java
@@ -0,0 +1,20 @@
+package com.gson.algo.leetcode.top100;
+
+/**
+ * https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-tree/
+ *
+ */
+public class 二叉树的最近公共祖先_236 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return lowestCommonAncestor1(root, p, q);
+    }
+
+    public TreeNode lowestCommonAncestor1(TreeNode root, TreeNode p, TreeNode q) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
