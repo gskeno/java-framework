c8838c0e900b3d910ac490a19bfb2bb8e7b77594
ruchen <ruchen.gs@alibaba-inc.com>
2022-08-17 23:33:50 +0800

    算法训练

--- java-algo/src/main/java/com/gson/algo/leetcode/网络延迟时间.java
+++ java-algo/src/main/java/com/gson/algo/leetcode/网络延迟时间.java
@@ -1,6 +1,6 @@
 package com.gson.algo.leetcode;
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * https://leetcode.cn/problems/network-delay-time/
@@ -63,6 +63,81 @@ public class 网络延迟时间 {
         return ans == INF ? -1 : ans;
     }
 
+    /**
+     * n个节点,
+     * times[i][j]表示节点i到节点j的值
+     * 第k个节点作为源节点
+     * @param times
+     * @param n
+     * @param k
+     * @return
+     */
+    public int networkDelayTime1(int[][] times, int n, int k) {
+        // 使用邻接表 来存储图
+        // table[i]表示以i节点为起点, 节点编号从0开始标记,
+        // 元素值是一个int[] 数组列表, 每一个元素int[],表示一个节点,设置为M。
+        // 节点i到节点M(编号为M[0])的最短路径长度为M[1]
+        List<int[]>[] table = new List[n];
+        for (int i = 0; i < n; i++) {
+            table[i] = new ArrayList<>();
+        }
+        for(int[] time : times){
+            // x到y节点的距离为dist
+            int x = time[0] - 1;
+            int y = time[1] - 1;
+            int dist = time[2];
+            // 表示x节点到y节点的距离为dist
+            table[x].add(new int[]{y, dist});
+        }
+        // 元素类型为int[], 表示的是一个节点，该节点到源点的最短路径长度为int[0], 该节点的id为int[1]
+        PriorityQueue<int[]> queue = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                // 以节点到源点的最短路径长度做最小堆
+                return o1[0] - o2[0];
+            }
+        });
+        queue.offer(new int[]{0, k-1});
+        // dist[i]表示i节点到源点的最短路径长度，初始化都为无穷大
+        int[] dist = new int[n];
+        int INF = Integer.MAX_VALUE/2;
+        Arrays.fill(dist, INF);
+        // 源点到源点的最短路径长度为0
+        dist[k-1] = 0;
+
+        while (!queue.isEmpty()){
+            int[] nearestNode = queue.poll();
+            // 最近节点到源点的距离
+            int nearDist = nearestNode[0];
+            // 最近节点的id
+            int x = nearestNode[1];
+
+            // 剪枝，这里表示从队列中取出的节点 距源点最短路径长度dist[x] 比当初offer进队列时小，
+            // 就是最短路径长度已经被更新过，这里不再更新了，因为同一个节点，可能多次进队列。
+            // 比如源点A到节点B的距离为1，A到C的距离为5,B到C的距离为2。poll A时，因为B,C与A相邻,
+            // 所以B(1),C(5)进队列；下一次会poll B, 更新C的最短路径长度为C(3)，且offer C(3)。
+            // 至此队列中有C(5),C(3)。C(3)先出队列，C(5)再出队列时，发现C到源点的最短路径长度已经
+            // 更新为3，自己的5已经失效，所以跳过
+            if (dist[x] < nearDist){
+                continue;
+            }
+
+            // x节点的相邻节点
+            for(int[] neighbor : table[x]){
+                int neighborId = neighbor[0];
+                int neighborDist = neighbor[1];
+                if (nearDist + neighborDist < dist[neighborId]){
+                    dist[neighborId] = nearDist + neighborDist;
+                    // 这里可能导致同一个节点多次进队列
+                    queue.offer(new int[]{dist[neighborId], neighborId});
+                }
+            }
+        }
+
+        int ans = Arrays.stream(dist).max().getAsInt();
+        return ans == INF ? -1 : ans;
+    }
+
     public static void main(String[] args) {
         test1();
         test2();
@@ -70,7 +145,7 @@ public class 网络延迟时间 {
 
     public static void test1(){
         网络延迟时间 solution = new 网络延迟时间();
-        int ans = solution.networkDelayTime(new int[][]{
+        int ans = solution.networkDelayTime1(new int[][]{
                 {2, 1, 1},
                 {2, 3, 1},
                 {3, 4, 1}
@@ -80,7 +155,7 @@ public class 网络延迟时间 {
 
     public static void test2(){
         网络延迟时间 solution = new 网络延迟时间();
-        int ans = solution.networkDelayTime(new int[][]{
+        int ans = solution.networkDelayTime1(new int[][]{
                 {1,2,1}
         }, 2, 2);
         System.out.println(ans);
