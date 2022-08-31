d122a4362d07a9413faf8c89ed6cdb76c92ab350
ruchen <ruchen.gs@alibaba-inc.com>
2022-08-31 19:55:44 +0800

    算法训练

--- java-mybatis/src/main/java/com/gson/mybatis/model/User.java
+++ java-mybatis/src/main/java/com/gson/mybatis/model/User.java
@@ -3,6 +3,10 @@ package com.gson.mybatis.model;
 public class User {
     private Integer id;
 
+    private String userId;
+
+    private String userName;
+
     public Integer getId() {
         return id;
     }
@@ -11,10 +15,28 @@ public class User {
         this.id = id;
     }
 
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
+                ", userId='" + userId + '\'' +
+                ", userName='" + userName + '\'' +
                 '}';
     }
 }
--- java-mybatis/src/test/java/com/gson/mybatis/AppTest.java
+++ java-mybatis/src/test/java/com/gson/mybatis/AppTest.java
@@ -5,8 +5,13 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Unit test for simple App.
@@ -14,11 +19,50 @@ import java.util.List;
 public class AppTest 
 {
     public static void main(String[] args) {
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:beans.xml");
+//        SqlSessionFactory sessionFactory = context.getBean(SqlSessionFactory.class);
+//
+//        SqlSession sqlSession = new SqlSessionTemplate(sessionFactory);
+//        List<Object> list = sqlSession.selectList("model.user.list", null);
+//        System.out.println(list);
+//        Object o = sqlSession.selectOne("model.user.query", "3");
+//        System.out.println(o);
+        test1();
+    }
+
+    public static void test1(){
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:beans.xml");
         SqlSessionFactory sessionFactory = context.getBean(SqlSessionFactory.class);
-
         SqlSession sqlSession = new SqlSessionTemplate(sessionFactory);
-        List<Object> list = sqlSession.selectList("model.user.list", null);
-        System.out.println(list);
+
+        TransactionTemplate template = context.getBean(TransactionTemplate.class);
+        Object result = template.execute(new TransactionCallback<Object>() {
+            @Override
+            public Object doInTransaction(TransactionStatus transactionStatus) {
+                Object o = null;
+                for (int i = 0; i < 5 ; i++) {
+                    String userId = "4";
+                    o = sqlSession.selectOne("model.user.query", userId);
+                    System.out.println(o);
+                    if (o == null){
+                        Map<String,Object> map = new HashMap<>();
+                        map.put("userId", userId);
+                        map.put("userName", userId);
+                        int insert = 0;
+                        try {
+                             insert = sqlSession.insert("model.user.insert", map);
+                        }catch (Exception e){
+                            System.out.println("写入失败");
+                        }
+                        if (insert > 0){
+                            System.out.println("写入成功");
+                        }
+                    }
+
+                }
+                return o;
+            }
+        });
+
     }
 }
