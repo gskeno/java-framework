6068325854688c334319a3ac3da94c0642e8d962
ruchen <ruchen.gs@alibaba-inc.com>
2022-09-04 19:42:23 +0800

    算法训练

--- java-jvm/src/main/java/com/gson/Main.java
+++ java-jvm/src/main/java/com/gson/Main.java
@@ -1,7 +1,8 @@
 package com.gson;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello world!");
+        Thread.sleep(100 *1000);
     }
 }
