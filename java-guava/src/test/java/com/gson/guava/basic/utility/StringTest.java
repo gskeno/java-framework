edc5134cbbe9adaf4543d5c2c1b2dd6f61959c8f
ruchen <ruchen.gs@alibaba-inc.com>
2022-10-17 12:13:43 +0800

    测试CaseFormat

--- java-guava/src/test/java/com/gson/guava/basic/utility/StringTest.java
+++ java-guava/src/test/java/com/gson/guava/basic/utility/StringTest.java
@@ -1,13 +1,15 @@
 package com.gson.guava.basic.utility;
 
+import com.google.common.base.CaseFormat;
+import com.google.common.base.CharMatcher;
 import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -59,5 +61,48 @@ public class StringTest {
         Splitter.MapSplitter mapSplitter1 = Splitter.on("&").withKeyValueSeparator(Splitter.on("="));
         Map<String, String> map1 = mapSplitter1.split("name=gs&age=20");
         Assert.assertEquals(map, map1);
+    }
+
+    @Test
+    public void testCharMatcher(){
+        // 分成两部分 1.满足筛选条件的子串 2.满足筛选条件的子串如何处理
+        // collapse有折叠的意思
+        // 任意的字符都能匹配，被-替代
+        Assert.assertEquals(CharMatcher.any().collapseFrom("bookstore", '-'), "-");
+        // 字符串中任意的k,o,e都能匹配，被-替代
+        Assert.assertEquals(CharMatcher.anyOf("koe").collapseFrom("bookstore", '-'),
+                "b-st-r-");
+        //'r'到't'之间的字符都能被-替代
+        Assert.assertEquals(CharMatcher.inRange('r','t').collapseFrom("bookstore", '-'),
+                "book-o-e");
+
+        Assert.assertEquals(CharMatcher.anyOf("o").replaceFrom("bookstore", "-"),
+                "b--kst-re");
+
+    }
+
+    @Test
+    public void testCharset(){
+        String student = new String("student");
+        student.getBytes(StandardCharsets.UTF_8);
+    }
+
+    /**
+     * 字符格式转换
+     */
+    @Test
+    public void testCaseFormat(){
+        // 小写驼峰转小写下划线
+        String s = CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, "myScore");
+        Assert.assertEquals(s, "my_score");
+        // 小写驼峰转小写连字符'-'
+        s = CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_HYPHEN, "myScore");
+        Assert.assertEquals(s, "my-score");
+        // 小写驼峰转大写驼峰(首字母大写)
+        s = CaseFormat.LOWER_CAMEL.to(CaseFormat.UPPER_CAMEL, "myScore");
+        Assert.assertEquals(s, "MyScore");
+        // 小写驼峰转大写下划线
+        s = CaseFormat.LOWER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, "myScore");
+        Assert.assertEquals(s, "MY_SCORE");
     }
 }
