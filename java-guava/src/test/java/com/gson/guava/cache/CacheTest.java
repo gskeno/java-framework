93a382d855b3992b200fd863242c0088921def32
ruchen <ruchen.gs@alibaba-inc.com>
2022-10-25 20:47:54 +0800

    guava cache

--- java-guava/src/test/java/com/gson/guava/cache/CacheTest.java
+++ java-guava/src/test/java/com/gson/guava/cache/CacheTest.java
@@ -1,9 +1,6 @@
 package com.gson.guava.cache;
 
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
+import com.google.common.cache.*;
 import com.google.common.reflect.Reflection;
 import com.gson.guava.User;
 import org.junit.Test;
@@ -74,6 +71,39 @@ public class CacheTest {
         loadingCacheBySize.get("B");
         // print "load A", because A not in cache, after, A->B->C
         loadingCacheBySize.get("A");
+    }
+
+
+    LoadingCache<String, String> loadingCacheByWeight = CacheBuilder.newBuilder()
+            .maximumWeight(100).weigher(new Weigher<String, String>() {
+                @Override
+                public int weigh(String key, String value) {
+                    System.out.println(key + " weight is " + key.length() * 10);
+                    return key.length() * 10;
+                }
+            }).build(new CacheLoader<String, String>() {
+                @Override
+                public String load(String key) throws Exception {
+                    System.out.println("load " + key);
+                    return key;
+                }
+            });
+
+    /**
+     * 之前理解有偏差，以为 首先逐出比重 相近的缓存对象，逐出这一个对象后，
+     * 新对象放进cache。
+     * 其实不然，这里只是提供了一个根据weight来计算元素何时达到 容量阈值 size而已
+     * @throws ExecutionException
+     */
+    @Test
+    public void testEvictByWeight() throws ExecutionException {
+        loadingCacheByWeight.get("ABCD");
+        loadingCacheByWeight.get("ABC");
+        loadingCacheByWeight.get("AB");
+        loadingCacheByWeight.get("A");
+        // put B，will evict "ABCD"
+        loadingCacheByWeight.get("B");
+        loadingCacheByWeight.get("ABCD");
     }
 
 }
