a561915918dbfada096629e024eff7a20c358886
ruchen <ruchen.gs@alibaba-inc.com>
2022-11-07 14:32:20 +0800

    powermock

---
+++ java-powermock/src/main/java/com/gson/power/mock/ThreadLocalCacheService.java
@@ -0,0 +1,8 @@
+package com.gson.power.mock;
+
+public class ThreadLocalCacheService implements CacheService{
+    @Override
+    public <V, K> V getValue(K k) {
+        return null;
+    }
+}
---
+++ java-powermock/src/test/java/com/gson/power/mock/MockTest.java
@@ -0,0 +1,12 @@
+package com.gson.power.mock;
+
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+
+@RunWith(PowerMockRunner.class)
+public class MockTest {
+
+    @Mock
+    private CacheService cacheService;
+
+}
