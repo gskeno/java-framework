be502da0f2a16f93e17adc028a391b4e9d8beb5e
ruchen <ruchen.gs@alibaba-inc.com>
2022-11-07 15:12:51 +0800

    powermock

--- java-powermock/src/test/java/com/gson/power/mock/MockTest.java
+++ java-powermock/src/test/java/com/gson/power/mock/MockTest.java
@@ -3,6 +3,7 @@ package com.gson.power.mock;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 @RunWith(PowerMockRunner.class)
 public class MockTest {
@@ -14,5 +15,4 @@ public class MockTest {
     public void test(){
         System.out.println("hello world");
     }
-
 }
