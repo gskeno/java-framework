5233c02031e84009409d595424c40ec5db1be6b0
ruchen <ruchen.gs@alibaba-inc.com>
2024-01-08 19:18:50 +0800

    测试二分法查询数据

--- java-jdk/src/test/java/com/gson/javajdk/math/ArraysTest.java
+++ java-jdk/src/test/java/com/gson/javajdk/math/ArraysTest.java
@@ -4,6 +4,12 @@ import java.util.Arrays;
 
 public class ArraysTest {
 
+    /**
+     * 当未命中时，返回 (-(insertion point) - 1).
+     * 这个插入点表示第一个比key大的元素在数组中的位置(从0开始)
+     * 特别的，当key比数组所有元素都大时，可以认为插入点在array.length
+     * @param args
+     */
     public static void main(String[] args) {
         int[] nums = new int[]{1, 7, 20, 100, 200};
         int index = Arrays.binarySearch(nums, 0);
