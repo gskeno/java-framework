5fec05d624af444219624df97336d55c2b3f9e95
gskeno <gsonkeno@sina.com>
2024-08-06 23:49:23 +0800

    make log4j2 mybatisplus valid

---
+++ java-mybatis-plus/src/test/java/com/gsonkeno/javamybatisplus/Log4j2Test.java
@@ -0,0 +1,12 @@
+package com.gsonkeno.javamybatisplus;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+@Slf4j
+public class Log4j2Test {
+    @Test
+    public void test(){
+        log.info("Log4j2Test test");
+    }
+}
